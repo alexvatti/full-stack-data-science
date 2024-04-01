b8ee31bb41fd5d08820ed809692228547a28aef1
Alex <alexvatti@gmail.com>
2024-04-01 13:05:34 +0530

    Add Streamlit app for salary prediction and deploy it Implemented a Streamlit web application for predicting salaries based on provided features. The app allows users to input various parameters and get a predicted salary as output. Deployment has been successfully completed, making the application accessible to users. This commit includes all necessary files and configurations for the Streamlit app deployment.

--- PythonModulesDataScience/07_Streamlit-Exercise/ML-Modles-Apps/Salary-Prediction/01_Salary-Prediction-App.py
+++ PythonModulesDataScience/07_Streamlit-Exercise/ML-Modles-Apps/Salary-Prediction/01_Salary-Prediction-App.py
@@ -3,25 +3,26 @@ import streamlit as st
 import pickle
 
 # give a title to our app
-st.title('Welcome to Salary Prediction Application')
+st.title('Welcome to Salary Prediction Application ')
 
 yrs_min = 1
 yrs_max = 11
 salary_min = 37731
 salary_max = 122391
 # TAKE yr_exp input in yrs
-st.image('salary-image.jpg', caption='salary')
-yrs_exp = st.number_input("Enter your Expereince (in Years)(Min-1 , Max-11)")
+st.image('salary-image.jpg')
+yrs_exp = st.number_input("Enter your Expereince (in Years)(Min-1 , Max-20)",min_value=1, max_value=20, value=3, step=1)
 
-if (yrs_exp >= 1) or (yrs_exp<=11):
+if (yrs_exp >= 1) and (yrs_exp<=20):
     pickled_model = pickle.load(open('salary-model.pkl', 'rb'))
     yrs = (yrs_exp - yrs_min)/ (yrs_max - yrs_min)
     ans=pickled_model.predict([[yrs]])
     salary = ans * (salary_max - salary_min) + salary_min
     if(st.button('Predict')):
         # print t
-        st.text("Salary is {}.".format(salary[0]))
+        st.header("Predicted Salary is {} per month".format(round(salary[0],2)))
 else:
-    st.text("Enter valid Input of Exp in Yrs")
+    st.header("Enter valid Input of Exp in Yrs")
+
 
 
---
+++ PythonModulesDataScience/07_Streamlit-Exercise/ML-Modles-Apps/Salary-Prediction/ReadMe.txt
@@ -0,0 +1,24 @@
+
+Step 1: Run the following command in the command prompt:
+pip install -r requirements.txt
+
+Step 2: Open the Jupyter Notebook (install Jupyter if not already installed).
+
+
+Step 3: Clone the code from the GitHub repository:
+https://github.com/alexvatti/full-stack-data-science/blob/main/Internship-Exercises/ML-Internship/Regression/Salary-Regression/Salary-Prediction-Regression.ipynb
+https://github.com/alexvatti/full-stack-data-science/blob/main/Internship-Exercises/ML-Internship/Regression/Salary-Regression/Salary_Data-Salary_Data.csv
+
+Step 4: Open the cloned directory with Jupyter and run all cells. This will generate "salary-model.pkl".
+
+Step 5: Open the salary-prediction app with VSCode.
+
+Step 6:
+
+1.Open the Anaconda command prompt.
+2.Navigate to the directory of the salary-prediction app:
+cd <path_of_the_salary-prediction>
+3. Run the Streamlit app: streamlit run 01_Salary-Prediction-App.py
+
+Step 7: A new webpage will be opened in the web browser.
+ Enter the input - number of years of experience and click on the predict button.
